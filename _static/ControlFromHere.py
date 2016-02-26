aec45c9ac0209ea311af2b1421b0668970e168da
djungelorm <djungelorm@users.noreply.github.com>
2016-02-25 22:31:25 -0800

    Update site from branch master (2531fb8b9c60030ec7a5069142a175d8322aa8a9)

---
+++ _static/ControlFromHere.py
@@ -0,0 +1,8 @@
+import krpc
+conn = krpc.connect()
+vessel = conn.space_center.active_vessel
+
+ports = vessel.parts.docking_ports
+port = list(filter(lambda p: p.part.title == 'Clamp-O-Tron Docking Port', ports))[0]
+part = port.part
+vessel.parts.controlling = part
---
+++ _static/InfernalRobotics.cs
@@ -0,0 +1,27 @@
+using KRPC.Client;
+using KRPC.Client.Services.InfernalRobotics;
+using System;
+using System.Threading;
+using System.Net;
+
+class InfernalRoboticsExample
+{
+    public static void Main ()
+    {
+        var connection = new KRPC.Client.Connection (name: "InfernalRobotics Example");
+        var ir = connection.InfernalRobotics ();
+
+        var group = ir.ServoGroupWithName ("MyGroup");
+        if (group == null) {
+            Console.WriteLine ("Group not found");
+            return;
+        }
+
+        foreach (var servo in group.Servos)
+            Console.WriteLine (servo.Name + " " + servo.Position);
+
+        group.MoveRight ();
+        Thread.Sleep (1000);
+        group.Stop ();
+    }
+}
